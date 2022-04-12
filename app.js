4cefc5e48e64e762ca8ebf688efc015f2e3abffc
Mason Wagoner <mgwagoner@gmail.com>
2022-04-11 22:09:19 -0400

    end day 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// Create the snake
-const snake = snake(100,110);
+// // Create the snake
+// const snake = snake();
 
-// Create a NPC
-const npc = newNPC(50, 300);
+// // Create a NPC
+// const npc = newNPC();
 
-// NPC movement
+// // NPC movement
