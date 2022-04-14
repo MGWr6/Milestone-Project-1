43d0afbd6eb9a4c025b03782c6228bda418d5ca3
Mason Wagoner <mgwagoner@gmail.com>
2022-04-14 17:11:52 -0400

    debugging console and NaN

--- app.js
+++ app.js
@@ -6,3 +6,35 @@
   - let player choose to play again
   */
 
+// Game Values
+let min = 1;
+let max = 10;
+let winningNum = 2 //need to create a function that will generate a random number
+let guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const guessBtn = document.querySelector('#guess-btn');
+const guessInput = document.querySelector('#guess-input');
+const message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Event Listener
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+  
+  // Validate
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`)
+  }
+});
+
+// Set Message Function
+function setMessage(msg){
+  message.textContent = msg;
+}
