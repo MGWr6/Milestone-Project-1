a8c386fa97df644377c2aa0d6c0b99c04c581ad5
Mason Wagoner <mgwagoner@gmail.com>
2022-04-13 20:33:18 -0400

    change of direction, outlined js functionality

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// // Create the snake
-// const snake = snake();
+/* GAME FUNCTION
+  - Player must guess a number between a min and max
+  - Player gets a certain amount of guesses
+  - Notify player of guesses remaining
+  - Notify the player of the correct answer if they lose
+  - let player choose to play again
+  */
 
-// // Create a NPC
-// const npc = newNPC();
-
-// // NPC movement
