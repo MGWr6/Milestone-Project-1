879a0688c25b87d544381f916063c6cab38aec0e
Mason Wagoner <mgwagoner@gmail.com>
2022-04-14 23:35:17 -0400

    set winning number and detailed comments

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let max = 10;
 let winningNum = 2 //need to create a function that will generate a random number
 let guessesLeft = 3;
 
+
 // UI Elements
 const game = document.querySelector('#game');
 const minNum = document.querySelector('.min-num');
@@ -20,21 +21,38 @@ const guessBtn = document.querySelector('#guess-btn');
 const guessInput = document.querySelector('#guess-input');
 const message = document.querySelector('.message');
 
+
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
 
 // Event Listener
 guessBtn.addEventListener('click', function(){
   let guess = parseInt(guessInput.value);
   
-  // Validate
+  // Validating a correct value was entered
+  // This functionality should ensure that only the parameters I've set can be entered. If a value outside the parameters is entered, or no value at all, the Message will display in red 'Please enter a number between " " and " ". This can be tested by entering any number outside 1-10 and clicking submit or entering no number at all and clicking submit
   if(isNaN(guess) || guess < min || guess > max){
-    setMessage(`Please enter a number between ${min} and ${max}`)
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // console.log(guessInput.value)
+
+
+  // Checking if player guessed the right number
+  // This functionality will create a green border around the box to let the player know they've won, with a message stating they've won in green as well! We can test this by entering the number 2, as indicated by winningNum variable above. (Will incorporate more advanced functionality later)
+  if(guess === winningNum){
+    guessInput.disabled = true;
+    guessInput.style.borderColor = 'green';
+    setMessage(`${winningNum} is Correct! YOU WON!`, 'green');
   }
 });
 
 // Set Message Function
-function setMessage(msg){
+function setMessage(msg, color){
+  message.style.color = color;
   message.textContent = msg;
 }
+
+// If player guesses wrong number, will notify player that their answer was not correct and tell them how many guesses they have left.
