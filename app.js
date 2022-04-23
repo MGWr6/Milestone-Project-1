dd6b03e1eeba71224d9fc922bf858c14a717fc1c
Mason Wagoner <mgwagoner@gmail.com>
2022-04-22 22:23:53 -0400

    Fin

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ game.addEventListener('mousedown', function(e){
 // Game Over
 function gameOver(won, msg){
   let color;
-  won === true ? color = 'white'  : color = 'red';
+  won === true ? color = 'green'  : color = 'red';
   
   // Disable Input
   guessInput.disabled = true;
