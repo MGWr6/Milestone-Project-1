adbe00b1f3536de407bbfc6564259e12e3687f1c
Mason Wagoner <mgwagoner@gmail.com>
2022-11-02 11:52:59 -0400

    cleaned up code and pushed up READme

---
+++ READme.md
@@ -0,0 +1,9 @@
+Our first milestone project assessed our current knowledge of JavaScript, HTML, and CSS and directed us to create browser-based game. This particular game is a number guesser. It is very simple in functionality but I feel it provides a solid grasp on my current understanding of JavaScript. I would like to improve upon this game in the future by adding animations, incentives to continue playing, and subtle cues to hint how close the player is to guessing the correct number.
+
+
+Pseudocode
+  - Player must guess a number between a set minimum and maximum number
+  - Player gets a certain amount of guesses
+  - Notify player of guesses remaining
+  - Notify the player of the correct answer if they lose
+  - let player choose to play again
--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-/* GAME FUNCTION
-  - Player must guess a number between a min and max
-  - Player gets a certain amount of guesses
-  - Notify player of guesses remaining
-  - Notify the player of the correct answer if they lose
-  - let player choose to play again
-  */
-
 // Game Values
 let min = 1;
 let max = 10;
-let winningNum = getRandomNumber(min, max); 
+let winningNum = getRandomNumber(min, max);
 let guessesLeft = 3;
-
 
 // UI Elements
 const game = document.querySelector('#game');
@@ -22,65 +13,67 @@ const guessInput = document.querySelector('#guess-input');
 const message = document.querySelector('.message');
 // const container = document.querySelector('.container')
 
-
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
 
-
 // Event Listener
-guessBtn.addEventListener('click', function(){
+guessBtn.addEventListener('click', function () {
   let guess = parseInt(guessInput.value);
   // console.log(guess);
-  
+
   // Validating a correct input was entered
   // This functionality should ensure that only the parameters I've set can be entered. If a value outside the parameters is entered, or no value at all, the Message will display in red 'Please enter a number between " " and " ". This can be tested by entering any number outside 1-10 and clicking submit or entering no number at all and clicking submit
-  
-  if(isNaN(guess) || guess < min || guess > max){
+
+  if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
   } else {
-
-  // console.log(guessInput.value)
-  // Checking if player guessed the right number
-  // This functionality will create a green border around the box to let the player know they've won, with a message stating they've won in green as well! We can test this by entering the number 2, as indicated by winningNum variable above. (Will incorporate more advanced functionality later)
-  if(guess === winningNum){
-  gameOver(true, `${winningNum} is correct! YOU WIN!!!`)
-  } else {
-    // Wrong Number
-    guessesLeft -= 1;
-
-    if(guessesLeft === 0){
-    // Game Over - you lose
-    gameOver(false, `Game Over, you lost. The correct number was ${winningNum}.`);
+    // console.log(guessInput.value)
+    // Checking if player guessed the right number
+    // This functionality will create a green border around the box to let the player know they've won, with a message stating they've won in green as well! We can test this by entering the number 2, as indicated by winningNum variable above. (Will incorporate more advanced functionality later)
+    if (guess === winningNum) {
+      gameOver(true, `${winningNum} is correct! YOU WIN!!!`);
     } else {
-      // Game Continues - answer wrong
+      // Wrong Number
+      guessesLeft -= 1;
 
-      // Change Border Color
-      guessInput.style.borderColor = 'red';
+      if (guessesLeft === 0) {
+        // Game Over - you lose
+        gameOver(
+          false,
+          `Game Over, you lost. The correct number was ${winningNum}.`
+        );
+      } else {
+        // Game Continues - answer wrong
 
-      // Disable Input
-      guessInput.value = '';
+        // Change Border Color
+        guessInput.style.borderColor = 'red';
 
-      // Tell user answer was incorrect
-      setMessage(`${guess} is not correct, ${guessesLeft} guesses left.`, 'red');
-    }
+        // Disable Input
+        guessInput.value = '';
+
+        // Tell user answer was incorrect
+        setMessage(
+          `${guess} is not correct, ${guessesLeft} guesses left.`,
+          'red'
+        );
+      }
     }
   }
 });
 
 // Play again event Listener
-game.addEventListener('mousedown', function(e){
-  if(e.target.className === 'play-again'){
+game.addEventListener('mousedown', function (e) {
+  if (e.target.className === 'play-again') {
     window.location.reload();
   }
-})
-
+});
 
 // Game Over
-function gameOver(won, msg){
+function gameOver(won, msg) {
   let color;
-  won === true ? color = 'green'  : color = 'red';
-  
+  won === true ? (color = 'green') : (color = 'red');
+
   // Disable Input
   guessInput.disabled = true;
   // Change border color
@@ -93,17 +86,16 @@ function gameOver(won, msg){
   // Play Again?
   guessBtn.value = 'Play Again?';
   guessBtn.className += 'play-again';
-
 }
 
 // Get Winning Number
-function getRandomNumber(min, max){
-  return Math.floor(Math.random()*(max-min+1)+min)
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
   // console.log(Math.floor(Math.random()*(max-min+1)+min)); // 10 takeaway 9 + 1 = 10
 }
 
 // Set Message Function
-function setMessage(msg, color){
+function setMessage(msg, color) {
   message.style.color = color;
   message.textContent = msg;
 }
